c4519a0557615b14773d26882a796259fcb6e395
mengstudaniel91-crypto <mengstudaniel91@gmail.com>
2026-03-24 01:33:53 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,13 +1,26 @@
+import os
 import requests
 
-def fetch_biotech_news():
-    # ለምሳሌ ስለ 'CRISPR' ወይም 'Vaccine' መረጃ መፈለግ ቢፈልጉ
+def fetch_and_send_news():
+    # 1. መረጃ መፈለጊያ (PubMed API ለምሳሌ)
     topic = "Biotechnology"
-    print(f"ለ ዳንኤል ስለ {topic} አዳዲስ መረጃዎችን እየፈለግኩ ነው...")
+    api_url = f"https://eutils.ncbi.nlm.nih.gov/entrez/eutils/esearch.fcgi?db=pubmed&term={topic}&retmode=json&retmax=3"
     
-    # ለጊዜው መረጃው ያለበትን ሊንክ እናሳይ
-    search_url = f"https://pubmed.ncbi.nlm.nih.gov/?term={topic}"
-    print(f"የጥናቶቹን ዝርዝር እዚህ ያገኙታል፦ {search_url}")
+    # 2. የቴሌግራም መለያዎች (ከ GitHub Secrets የሚመጡ)
+    bot_token = os.getenv('BOT_TOKEN')
+    chat_id = os.getenv('CHAT_ID')
+    
+    response = requests.get(api_url).json()
+    ids = response['esearchresult']['idlist']
+    
+    message = "🧬 **የዛሬው የባዮቴክኖሎጂ አዳዲስ ጥናቶች፦**\n\n"
+    for pubmed_id in ids:
+        link = f"https://pubmed.ncbi.nlm.nih.gov/{pubmed_id}/"
+        message += f"• አዲስ ጥናት ተገኝቷል፦ {link}\n"
+
+    # 3. ወደ ቴሌግራም መላክ
+    send_url = f"https://api.telegram.org/bot{bot_token}/sendMessage"
+    requests.post(send_url, data={'chat_id': chat_id, 'text': message, 'parse_mode': 'Markdown'})
 
 if __name__ == "__main__":
-    fetch_biotech_news()
+    fetch_and_send_news()
